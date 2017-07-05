eee255d67ab13e38fb881a86e4452a381d642c9a
akash705 <ultimateakash705@gmail.com>
2017-07-05 01:01:13 -0700

    connection on and off handler added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express=require('express');
-var socket=require('socket.io');
+var socketio=require('socket.io');
 const path = require('path');
+var http=require('http');
 
 
 var app=express();
@@ -8,13 +9,24 @@ const port=process.env.PORT || 3000 ;
 const publicpath=path.join(__dirname,'../public');
 
 app.use(express.static(publicpath));
+var server=http.createServer(app);
+
+var io=socketio(server);
+
+io.on("connect",(socket)=>
+{
+    console.log("new user connected");
+    socket.on('disconnect',()=>{
+        console.log("user was disconnect");
+    });
+});
 
 app.get('/',(req,res)=>
 {
   res.send("hell");
 })
 
-app.listen(port,()=>{
+server.listen(port,()=>{
   console.log(`Server is live go to localhost:${port}`);
 
 })
