8cd9ce1d344d7e9bcdb340b81b8109a6d228a6c8
akash705 <ultimateakash705@gmail.com>
2017-07-04 22:52:14 -0700

    'commit'

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express=require('express');
+
+const path = require('path');
+
+
+var app=express();
+const port=process.env.PORT || 3000
+const publicpath=path.join(__dirname,"../public");
+
+app.use(express.static(publicpath));
+
+app.listen(port,()=>{
+  console.log(`Server is live go to localhost:${port}`);
+
+})
